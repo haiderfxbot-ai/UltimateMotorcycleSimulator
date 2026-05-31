6b895ca0408066f92b0013680253c68f742ed0b3
root <root@localhost.localdomain>
2026-05-31 19:50:42 +0000

    Fix macro usage: SYSTEM_ERROR not debug::SYSTEM_ERROR

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,4 @@
 #include "engine/GameEngine.h"
-#include "engine/InputManager.h"
-#include "renderer/Renderer.h"
-#include "motorcycle/Motorcycle.h"
-#include "world/World.h"
 #include "debug/ErrorLogger.h"
 #include "debug/RuntimeGuard.h"
 
@@ -21,7 +17,7 @@ int main(int argc, char* argv[]) {
 
     if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_GAMECONTROLLER) < 0) {
         SDL_Log("SDL init failed: %s", SDL_GetError());
-        debug::SYSTEM_ERROR("SDL initialization failed",
+        SYSTEM_ERROR("SDL initialization failed",
             "SDL_Init returned error",
             "Check SDL installation or video driver");
         return 1;
@@ -30,7 +26,7 @@ int main(int argc, char* argv[]) {
     GameEngine engine;
     if (!engine.init(1280, 720, "Ultimate Motorcycle Simulator")) {
         SDL_Log("Engine init failed");
-        debug::ENGINE_ERROR("GameEngine initialization failed",
+        ENGINE_ERROR("GameEngine initialization failed",
             "Renderer or world init returned false",
             "Check init() order and dependencies");
         SDL_Quit();
